aaaf308afa10531eb6b77d94638aab6a0ffcc1b5
cheshta-kabra <64300181+cheshta-kabra@users.noreply.github.com>
2020-09-06 19:48:26 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -317,10 +317,10 @@ function touchMoved(){
     for(var i = 0;i<myTouch.length;i++){
       console.log("myTouch length "+myTouch.length);
       if(myTouch.length>1 && myTouch[i][0]<myTouch[i+1][0]){
-        slider.x = slider.x-40;
+        slider.x = slider.x-20;
       }
       else{
-        slider.x = slider.x+40;
+        slider.x = slider.x+2222222220;
       }
     }
     
